17af652d43f23dd4a0fd30b8d77b88665f0d5925
Bruno Jorge Nogueira Batista <brunojbatista@hotmail.com>
2022-05-23 21:42:13 -0300

    Junção dos arquivos JS usando GulpJS

---
+++ _files/js/utils.js
@@ -0,0 +1,18 @@
+// Constantes
+const navBarHeight = 64;
+
+function formatCurrency(value, fractionDigits = 2) {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: fractionDigits }).format(value);
+}
+
+function scrollTo(top, left = 0, time = 50) {
+  setTimeout(
+    () => {
+      document.querySelector('#scroll-content').scrollTo({
+        top,
+        left,
+        behavior:'smooth'
+      })
+    }, time
+  )
+}
